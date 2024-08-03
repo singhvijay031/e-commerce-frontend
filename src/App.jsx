325147194ecea82e12476516597eff20bf78b26e
Bijay <bijaymahato031@gmail.com>
2024-08-03 20:15:58 +0545

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
