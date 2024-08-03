c16c3205b8b8de28182780096c7ae887bb3cd33d
Bijay <bijaymahato031@gmail.com>
2024-08-03 20:13:33 +0545

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
