b4a053b2d55a135a7f9136d1b47f012f62f088d2
Bijay <bijaymahato031@gmail.com>
2024-08-04 16:09:26 +0545

    updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import { Categories } from "../assets/mockData";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>SHOP BY CATEGORIES</div>
+            <ul>
+              {Categories.map((category, index) => (
+                <li key={index}>{category}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
