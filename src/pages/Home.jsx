fe4ad4513c6cff021ecca00add244fbf3895472d
Bijay <bijaymahato031@gmail.com>
2024-08-04 22:40:04 +0545

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { setProducts } from "../redux/ProductSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import ProductCard from "../components/ProductCard";
+import Shop from "./Shop";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -16,51 +17,57 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
-      <div className="container mx-auto py-4 flex flex-col md:flex-row space-x-2">
-        <div className="w-full md:w-3/12">
-          <div className="bg-red-600 text-white text-xs font-bold px-2 py-2.5">
-            SHOP BY CATEGORIES
+    <div>
+      <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
+        <div className="container mx-auto py-4 flex flex-col md:flex-row space-x-2">
+          <div className="w-full md:w-3/12">
+            <div className="bg-red-600 text-white text-xs font-bold px-2 py-2.5">
+              SHOP BY CATEGORIES
+            </div>
+            <ul className="space-y-4 bg-gray-100 p-3 border">
+              {Categories.map((category, index) => (
+                <li
+                  key={index}
+                  className="flex items-center text-sm font-medium"
+                >
+                  <div className="w-2 h-2 border border-red-500 rounded-full mr-2"></div>
+                  {category}
+                </li>
+              ))}
+            </ul>
           </div>
-          <ul className="space-y-4 bg-gray-100 p-3 border">
-            {Categories.map((category, index) => (
-              <li key={index} className="flex items-center text-sm font-medium">
-                <div className="w-2 h-2 border border-red-500 rounded-full mr-2"></div>
-                {category}
-              </li>
+
+          <div className="w-full md:w-9/12 mt-8 md:mt-0 h-96 relative">
+            <img
+              src={Hero}
+              alt="Hero-Image"
+              className="h-full w-full object-cover"
+            />
+            <div className="absolute top-16 left-8">
+              <p className="font-bold mb-4">Vijay Fancy Store</p>
+              <h2 className="text-3xl font-bold">Welcome To E-SHOP</h2>
+              <p className="text-xl mt-2.5 font-bold text-gray-800">
+                MILLIONS + PRODUCTS
+              </p>
+              <button className="bg-red-600 px-8 py-1.5 text-white mt-4 hover:bg-red-700 transform transition-transform duration-300 hover:scale-105">
+                SHOP NOW
+              </button>
+            </div>
+          </div>
+        </div>
+        <InfoSection />
+        <CategorySection />
+
+        <div className=" container mx-auto py-12">
+          <h2 className=" text-2xl font-bold mb-6 text-center">Top Products</h2>
+          <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
+            {products.slice(0, 5).map((product, index) => (
+              <ProductCard key={index} product={product} />
             ))}
-          </ul>
-        </div>
-
-        <div className="w-full md:w-9/12 mt-8 md:mt-0 h-96 relative">
-          <img
-            src={Hero}
-            alt="Hero-Image"
-            className="h-full w-full object-cover"
-          />
-          <div className="absolute top-16 left-8">
-            <p className="font-bold mb-4">Vijay Fancy Store</p>
-            <h2 className="text-3xl font-bold">Welcome To E-SHOP</h2>
-            <p className="text-xl mt-2.5 font-bold text-gray-800">
-              MILLIONS + PRODUCTS
-            </p>
-            <button className="bg-red-600 px-8 py-1.5 text-white mt-4 hover:bg-red-700 transform transition-transform duration-300 hover:scale-105">
-              SHOP NOW
-            </button>
           </div>
         </div>
       </div>
-      <InfoSection />
-      <CategorySection />
-
-      <div className=" container mx-auto py-12">
-        <h2 className=" text-2xl font-bold mb-6 text-center">Top Products</h2>
-        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
-          {products.slice(0, 5).map((product, index) => (
-            <ProductCard key={index} product={product} />
-          ))}
-        </div>
-      </div>
+      <Shop />
     </div>
   );
 };
