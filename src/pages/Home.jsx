1b90a2ddbcd3b2e4bffcaf3b40c6584f4f51d36a
Bijay <bijaymahato031@gmail.com>
2024-08-04 17:33:03 +0545

    added css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,25 +3,36 @@ import Hero from "../assets/images/hero-img.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <div>SHOP BY CATEGORIES</div>
-            <ul>
-              {Categories.map((category, index) => (
-                <li key={index}>{category}</li>
-              ))}
-            </ul>
+    <div className=" bg-white mt-2 px-4 md:px-16 lg:px-24">
+      <div className="container mx-auto py-4 flex flex-col md:flex-row space-x-2">
+        <div className=" w-full  md:w-3/12">
+          <div className=" bg-red-600 text-white text-xs font-bold px-2 py-2.5">
+            SHOP BY CATEGORIES
           </div>
-          <div>
-            <img src={Hero} alt="" />
-            <div>
-              <p>Vijay | Fancy Store</p>
-              <h2>Welcome to e-Shop </h2>
-              <p>MILLIONS + PRODUCTS</p>
-              <button>SHOP NOW</button>
-            </div>
+          <ul className=" space-y-4 bg-gray-100 p-3 border">
+            {Categories.map((category, index) => (
+              <li
+                key={index}
+                className=" flex items-center text-sm font-medium"
+              >
+                <div className=" w-2 h-2 border border-red-500 rounded-full mr-2 "></div>
+                {category}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className=" w-full md:w-9/12 mt-8 md:mt-0 h-96 relative">
+          <img src={Hero} alt="Hero-Image" className=" h-full w-full" />
+          <div className=" absolute top-16 left-8">
+            <p className="  font-bold mb-4">Vijay | Fancy Store</p>
+            <h2 className=" text-3xl font-bold">Welcome to e-Shop </h2>
+            <p className=" text-xl mt-2.5 font-bold text-gray-800">
+              MILLIONS + PRODUCTS
+            </p>
+            <button className=" bg-red-600 px-8 py-1.5 text-white mt-4 hover:bg-red-700 transform transition-transform duration-300 hover:scale-105">
+              SHOP NOW
+            </button>
           </div>
         </div>
       </div>
