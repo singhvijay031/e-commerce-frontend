4c575ac777ccef22bf6b63a4be24ecd18032fd3e
Bijay <bijaymahato031@gmail.com>
2024-08-04 22:13:23 +0545

    updated css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,9 +53,9 @@ const Home = () => {
       <InfoSection />
       <CategorySection />
 
-      <div>
-        <h2>Top Products</h2>
-        <div>
+      <div className=" container mx-auto py-12">
+        <h2 className=" text-2xl font-bold mb-6 text-center">Top Products</h2>
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
           {products.slice(0, 5).map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
