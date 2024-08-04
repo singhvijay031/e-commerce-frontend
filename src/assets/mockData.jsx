f57fd2e971a5faa320102bc23acf63f7f725c034
Bijay <bijaymahato031@gmail.com>
2024-08-04 19:30:40 +0545

    added mockData

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,3 +1,14 @@
+import Image1 from "./images/bag.jpeg";
+import Image2 from "./images/boot.jpeg";
+import Image3 from "./images/boyshirt.jpeg";
+import Image4 from "./images/cap.jpeg";
+import Image5 from "./images/dove.jpeg";
+import Image6 from "./images/headphone.jpeg";
+import Image7 from "./images/purse.jpeg";
+import Image8 from "./images/sandle.jpeg";
+import Image9 from "./images/robot.jpeg";
+import Image10 from "./images/waterbottle.jpeg";
+
 export const Categories = [
   "Electronics",
   "Fashion",
@@ -5,4 +16,67 @@ export const Categories = [
   "Beauty",
   "Sports",
   "Automotive",
+];
+
+export const mockData = [
+  {
+    id: 1,
+    image: Image9,
+    name: "Product 1",
+    price: 500,
+  },
+  {
+    id: 2,
+    image: Image8,
+    name: "Product 2",
+    price: 300,
+  },
+  {
+    id: 3,
+    image: Image7,
+    name: "Product 3",
+    price: 100,
+  },
+  {
+    id: 4,
+    image: Image6,
+    name: "Product 4",
+    price: 800,
+  },
+  {
+    id: 5,
+    image: Image5,
+    name: "Product 5",
+    price: 200,
+  },
+  {
+    id: 6,
+    image: Image4,
+    name: "Product 6",
+    price: 200,
+  },
+  {
+    id: 7,
+    image: Image3,
+    name: "Product 7",
+    price: 700,
+  },
+  {
+    id: 8,
+    image: Image2,
+    name: "Product 8",
+    price: 1500,
+  },
+  {
+    id: 9,
+    image: Image1,
+    name: "Product 9",
+    price: 500,
+  },
+  {
+    id: 10,
+    image: Image10,
+    name: "Product 10",
+    price: 150,
+  },
 ];
