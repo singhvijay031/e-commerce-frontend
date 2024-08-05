b793f4df1e11f6eae71632f57c7c6c5c7c0c6f87
Bijay <bijaymahato031@gmail.com>
2024-08-05 22:06:14 +0545

    designed ChangeAddress

--- src/components/ChangeAddress.jsx
+++ src/components/ChangeAddress.jsx
@@ -1,5 +1,21 @@
 const ChangeAddress = () => {
-  return <div>ChangeAddress</div>;
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter new address"
+        className=" border p-2 w-full mb-4"
+      />
+      <div className=" flex justify-end">
+        <button className=" bg-gray-500 text-white py2 px-4 rounded mr-2">
+          Cancel
+        </button>
+        <button className=" bg-blue-500 text-white py-2 px-4 rounded">
+          Save Address
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ChangeAddress;
