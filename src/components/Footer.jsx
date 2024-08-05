b34f0ef6abd7a9fcbdc20b22aa8fc99e0c9c1e37
Bijay <bijaymahato031@gmail.com>
2024-08-05 11:13:59 +0545

    updated css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -69,7 +69,7 @@ const Footer = () => {
       <div className=" mt-8 border-t border-gray-700 pt-4">
         <div className=" container mx-auto flex flex-col md:flex-row justify-between items-center">
           <p>&copy; 2024 e-Shop All rights reserved</p>
-          <div className=" flex space-x-4 mt-4 md:mt-0">
+          <div className=" flex space-x-4 mt-0 md:mt-0">
             <a href="" className=" hover:underline">
               Privacy Policy
             </a>
