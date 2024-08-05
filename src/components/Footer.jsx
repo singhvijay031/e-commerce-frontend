77ee312de58f3f9117133945982bcba0d8762bf9
Bijay <bijaymahato031@gmail.com>
2024-08-05 11:08:30 +0545

    updated css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -66,6 +66,19 @@ const Footer = () => {
           </form>
         </div>
       </div>
+      <div className=" mt-8 border-t border-gray-700 pt-4">
+        <div className=" container mx-auto flex flex-col md:flex-row justify-between items-center">
+          <p>&copy; 2024 e-Shop All rights reserved</p>
+          <div className=" flex space-x-4 mt-4 md:mt-0">
+            <a href="" className=" hover:underline">
+              Privacy Policy
+            </a>
+            <a href="" className=" hover:underline">
+              Terms & Conditions
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
