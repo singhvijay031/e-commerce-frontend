8142275125738b5170312384f16978232b0c15f3
Bijay <bijaymahato031@gmail.com>
2024-08-06 23:40:30 +0545

    changed the payment method

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -5,7 +5,7 @@ const CheckOut = () => {
   const [billingToggle, setBillingToggle] = useState(true);
   const [shippingToggle, setShippingToggle] = useState(false);
   const [paymentToggle, setPaymentToggle] = useState(false);
-  const [paymentMethod, setPaymentMethod] = useState("card");
+  const [paymentMethod, setPaymentMethod] = useState();
 
   return (
     <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
