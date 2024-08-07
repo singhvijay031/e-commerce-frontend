67b85705ccf1f59b2d33ba497a4b7a6316b9f60d
Bijay <bijaymahato031@gmail.com>
2024-08-08 00:40:09 +0545

    added route for continue shopping

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,5 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 const Order = ({ order }) => {
+  const navigate = useNavigate();
   return (
     <div className=" container mx-auto py-8 px-4 md:px-16 lg:px-24">
       <h2 className=" text-2xl font-semibold mb-4">
@@ -37,7 +40,10 @@ const Order = ({ order }) => {
           <button className=" bg-green-500 text-white py-2 px-4 hover:bg-gray-600 rounded">
             Order Tracking
           </button>
-          <button className=" ml-4 bg-red-600 text-white py-2 px-4 hover:bg-red-800 rounded">
+          <button
+            className=" ml-4 bg-red-600 text-white py-2 px-4 hover:bg-red-800 rounded"
+            onClick={() => navigate("/")}
+          >
             Continue Shopping
           </button>
         </div>
