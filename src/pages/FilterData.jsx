e84188675dc369c548871b175f1f625cb7612531
Bijay <bijaymahato031@gmail.com>
2024-08-09 22:56:35 +0545

    updated

--- src/pages/FilterData.jsx
+++ src/pages/FilterData.jsx
@@ -1,5 +1,27 @@
+import { useSelector } from "react-redux";
+import ProductCard from "../components/ProductCard";
+import NoProduct from "../assets/images/not_found.jpeg";
+
 const FilterData = () => {
-  return <div>FilterData</div>;
+  const filterProducts = useSelector((state) => state.product.filteredData);
+  return (
+    <div className="mx-auto py-12 px-4 md:px-16 lg:px-24">
+      {filterProducts.length > 0 ? (
+        <>
+          <h2 className="text-2xl font-bold mb-6 text-center">Shop</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
+            {filterProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className=" flex justify-center">
+          <img src={NoProduct} alt="" />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default FilterData;
