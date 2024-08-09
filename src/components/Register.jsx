e32cbd3227b522a78b05ce75910366ed5fdb22bc
Bijay <bijaymahato031@gmail.com>
2024-08-09 21:59:47 +0545

    designed Login and Register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,49 @@
+/* eslint-disable react/no-unescaped-entities */
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <h2 className=" text-2xl font-bold mb-4">Sign Up</h2>
+      <form>
+        <div className=" mb-4">
+          <label className=" block text-gray-700">Name</label>
+          <input
+            type="text"
+            className=" w-full px-3 py-2 border"
+            placeholder="Enter Name"
+          />
+        </div>
+
+        <div className=" mb-4">
+          <label className=" block text-gray-700">Email</label>
+          <input
+            type="email"
+            className=" w-full px-3 py-2 border"
+            placeholder="Enter Email"
+          />
+        </div>
+
+        <div className=" mb-4">
+          <label className=" block text-gray-700">Password</label>
+          <input
+            type="password"
+            className=" w-full px-3 py-2 border"
+            placeholder="Enter Password"
+          />
+        </div>
+
+        <div className=" mb-4">
+          <button type="submit" className=" w-full bg-red-600 text-white py-2">
+            Sign Up
+          </button>
+        </div>
+      </form>
+
+      <div className=" text-center">
+        <span className=" text-gray-700">Already Have an Account?</span>
+        <button className=" text-red-800">Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
